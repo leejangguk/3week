87b7686efe510beea914e45df0a4d90a5d2ce22b
leejangguk <ljg3603@daum.net>
2023-01-13 14:32:17 +0900

    dd

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ class Post(models.Model):
     # 데이터베이스의 필드(컬럼): 최대 길이가 30
     # title = models.CharField(max_length=30)
     # content = models.TextField()
+    #dd
 
     # created_at = models.DateTimeField()
     # author는 추후 작성
